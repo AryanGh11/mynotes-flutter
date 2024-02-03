ced30ef57a7acb0b069466ac5c3933c574afbad2
Aryan Gholami <mahdi.gholami875@gmail.com>
2024-02-03 13:34:57 +0330

    some ui changes

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -35,78 +35,80 @@ class _RegisterViewState extends State<RegisterView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Register")),
-      body: Column(
-        children: [
-          SimpleTextField(
-            hintText: "Enter your email",
-            textEditingController: _email,
-            enableSuggestions: false,
-            autocorrect: false,
-            textInputType: TextInputType.emailAddress,
+        appBar: AppBar(title: const Text("Register")),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              SimpleTextField(
+                hintText: "Enter your email",
+                textEditingController: _email,
+                enableSuggestions: false,
+                autocorrect: false,
+                textInputType: TextInputType.emailAddress,
+              ),
+              const SizedBox(height: 8),
+              SimpleTextField(
+                hintText: "Enter your password",
+                textEditingController: _password,
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+              ),
+              const SizedBox(height: 16),
+              SimpleElevatedButton(
+                onPressed: () async {
+                  final email = _email.text;
+                  final password = _password.text;
+                  try {
+                    await AuthService.firebase().createUser(
+                      email: email,
+                      password: password,
+                    );
+                    await AuthService.firebase().sendEmailVerification();
+                    // ignore: use_build_context_synchronously
+                    Navigator.of(context).pushNamed(
+                      verifyEmailRoute,
+                    );
+                  } on WeakPasswordAuthExeption {
+                    // ignore: use_build_context_synchronously
+                    await showErrorDialog(
+                      context,
+                      "Your password is weak",
+                    );
+                  } on EmailAlreadyInUseAuthExeption {
+                    // ignore: use_build_context_synchronously
+                    await showErrorDialog(
+                      context,
+                      "Email is already in use",
+                    );
+                  } on InvalidEmailAuthExeption {
+                    // ignore: use_build_context_synchronously
+                    await showErrorDialog(
+                      context,
+                      "This email is invalid",
+                    );
+                  } on GenericAuthExeption {
+                    // ignore: use_build_context_synchronously
+                    await showErrorDialog(
+                      context,
+                      "Failed to register",
+                    );
+                  }
+                },
+                child: const Text("Register"),
+              ),
+              const SizedBox(height: 8),
+              SimpleOutlinedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      loginRoute,
+                      (route) => false,
+                    );
+                  },
+                  child: const Text("Do you have an account? Login here!"))
+            ],
           ),
-          const SizedBox(height: 8),
-          SimpleTextField(
-            hintText: "Enter your password",
-            textEditingController: _password,
-            obscureText: true,
-            enableSuggestions: false,
-            autocorrect: false,
-          ),
-          const SizedBox(height: 16),
-          SimpleElevatedButton(
-            onPressed: () async {
-              final email = _email.text;
-              final password = _password.text;
-              try {
-                await AuthService.firebase().createUser(
-                  email: email,
-                  password: password,
-                );
-                await AuthService.firebase().sendEmailVerification();
-                // ignore: use_build_context_synchronously
-                Navigator.of(context).pushNamed(
-                  verifyEmailRoute,
-                );
-              } on WeakPasswordAuthExeption {
-                // ignore: use_build_context_synchronously
-                await showErrorDialog(
-                  context,
-                  "Your password is weak",
-                );
-              } on EmailAlreadyInUseAuthExeption {
-                // ignore: use_build_context_synchronously
-                await showErrorDialog(
-                  context,
-                  "Email is already in use",
-                );
-              } on InvalidEmailAuthExeption {
-                // ignore: use_build_context_synchronously
-                await showErrorDialog(
-                  context,
-                  "This email is invalid",
-                );
-              } on GenericAuthExeption {
-                // ignore: use_build_context_synchronously
-                await showErrorDialog(
-                  context,
-                  "Failed to register",
-                );
-              }
-            },
-            child: const Text("Register"),
-          ),
-          const SizedBox(height: 8),
-          SimpleOutlinedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  loginRoute,
-                  (route) => false,
-                );
-              },
-              child: const Text("Do you have an account? Login here!"))
-        ],
-      ),
-    );
+        ));
   }
 }
